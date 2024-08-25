59cf1cace4c238b41290f4ce54167ba09e4f2760
zhanglei_zl <zhanglei_zl@youzan.com>
2024-08-25 13:53:38 +0800

    feat: 修改sh命令

--- scripts/deployInUbuntu.sh
+++ scripts/deployInUbuntu.sh
@@ -1,29 +1,25 @@
-# 获取当前脚本所在目录
-$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-
-# 设置当前工作目录为脚本所在目录
-Set-Location -Path $scriptDir
+#!/bin/bash
 
 # 1. Git pull
-Write-Output "Pulling latest changes from git..."
+echo "Pulling latest changes from git..."
 git stash
 git pull
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Git pull failed. Exiting..."
+if [ $? -ne 0 ]; then
+    echo "Git pull failed. Exiting..."
     exit 1
-}
+fi
 
 # 2. Docker run
-Write-Output "try to run docker..."
+echo "Trying to run docker..."
 sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker system prune -a -f
-# rm  ~/.docker/config.json 
+# rm ~/.docker/config.json
 sudo docker compose pull
 sudo docker compose up -d
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Docker run failed. Exiting..."
+if [ $? -ne 0 ]; then
+    echo "Docker run failed. Exiting..."
     exit 1
-}
+fi
 
-Write-Output "All steps completed successfully."
+echo "All steps completed successfully."
