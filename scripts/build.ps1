98c20fa431886d37afbabc9515f6c057c56756ed
zhanglei_zl <zhanglei_zl@youzan.com>
2024-08-27 17:51:55 +0800

    feat:需要清理当前的镜像

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -16,7 +16,7 @@ if ($LASTEXITCODE -ne 0) {
 # 2. Docker run
 Write-Output "try to run docker..."
 sudo docker stop $(sudo docker ps -aq)
-# sudo docker rm $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 # sudo docker system prune -a -f
 sudo docker volume rm briar-mono_briar-static
 sudo docker compose pull
