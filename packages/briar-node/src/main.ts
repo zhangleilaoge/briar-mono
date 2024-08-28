633515f7acb45eb3b6e8903f9acabb97b6d51cc5
zhanglei_zl <zhanglei_zl@youzan.com>
2024-08-28 21:54:19 +0800

    feat: 添加ssl证书

--- packages/briar-node/src/main.ts
+++ packages/briar-node/src/main.ts
@@ -5,24 +5,39 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { isDev } from './constants/env';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const origins = [
+  /http:\/\/(www\.)?restrained-hunter\.website/,
+  /https:\/\/(www\.)?restrained-hunter\.website/,
+  /http:\/\/localhost:5173/,
+  /https:\/\/localhost:5173/,
+  /http:\/\/127\.0\.0\.1:5173/,
+  /https:\/\/127\.0\.0\.1:5173/,
+  /http:\/\/122\.51\.158\.41/,
+  /https:\/\/122\.51\.158\.41/,
+];
+
+const httpsOptions = {
+  key: fs.readFileSync(
+    path.join(__dirname, '../../../config/restrained-hunter.website.key'),
+  ),
+  cert: fs.readFileSync(
+    path.join(__dirname, '../../../config/restrained-hunter.website.crt'),
+  ),
+  ca: fs.readFileSync(path.join(__dirname, '../../../config/root_bundle.crt')),
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     MainModule,
     new FastifyAdapter(),
+    { httpsOptions },
   );
 
   app.enableCors({
-    origin: [
-      /http:\/\/(www\.)?restrained-hunter\.website/,
-      /https:\/\/(www\.)?restrained-hunter\.website/,
-      /http:\/\/localhost:5173/,
-      /https:\/\/localhost:5173/,
-      /http:\/\/127\.0\.0\.1:5173/,
-      /https:\/\/127\.0\.0\.1:5173/,
-      /http:\/\/122\.51\.158\.41/,
-      /https:\/\/122\.51\.158\.41/,
-    ],
+    origin: origins,
     methods: 'GET,PUT,POST',
     allowedHeaders: 'Content-Type,Authorization',
     exposedHeaders: [
