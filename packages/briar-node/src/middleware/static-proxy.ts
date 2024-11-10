6ba92ae8f811d150d59e8ab982ed103856a78e9f
zhanglei_zl <zhanglei_zl@youzan.com>
2024-11-10 21:13:08 +0800

    feat: 支持动态模版

--- packages/briar-node/src/middleware/static-proxy.ts
+++ packages/briar-node/src/middleware/static-proxy.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import * as ejs from 'ejs';
 import * as fs from 'fs';
 import { join } from 'path';
-
 @Injectable()
 export class StaticProxyMiddleware implements NestMiddleware {
   constructor() {}
@@ -22,7 +22,21 @@ export class StaticProxyMiddleware implements NestMiddleware {
 
       // 检查文件是否存在
       if (fs.existsSync(filePath)) {
-        fs.createReadStream(filePath).pipe(res);
+        const htmlContent = fs.readFileSync(filePath, 'utf-8');
+
+        // 渲染 EJS 模板
+        const data = { htmlContent };
+
+        // 暂时写死，如果有动态模版需求再作调整
+        ejs.renderFile('./src/views/briar.ejs', data, {}, (err, str) => {
+          if (err) {
+            console.error(err);
+            next();
+          }
+          // 手动写入响应并结束
+          res.write(str);
+          res.end();
+        });
       } else {
         next();
       }
