10896ba96695719a5be0ed5238f2494278289a5f
zhanglei_zl <zhanglei_zl@youzan.com>
2024-12-24 15:37:35 +0800

    feat: 使用定位更准确的ip定位包

--- packages/briar-node/src/services/IpService.ts
+++ packages/briar-node/src/services/IpService.ts
@@ -1,29 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import * as geoip from 'geoip-lite';
-
-interface IpLocationResponse {
-  country: string;
-  city: string;
-  timezone: string;
-}
+import IP2Region from 'ip2region';
 
 @Injectable()
 export class IpLocationService {
+  query: IP2Region = new IP2Region();
+
   constructor() {}
 
   async getIpLocation(ip: string): Promise<string> {
-    const locationData = await this.fetchLocation(ip);
+    const address = this.query.search(ip);
 
-    return `${locationData?.country} ${locationData?.timezone} ${locationData?.city}`;
-  }
-
-  private async fetchLocation(ip: string): Promise<IpLocationResponse | null> {
-    try {
-      const response = geoip.lookup(ip);
-      return response;
-    } catch (error) {
-      console.error('Error fetching IP location', error);
-      return null;
-    }
+    return JSON.stringify(address);
   }
 }
