efa56cd871294e5d117a0f97ef215337282a74de
zhanglei_zl <zhanglei_zl@youzan.com>
2025-04-15 19:59:21 +0800

    feat: 修打包问题

--- packages/briar-frontend/src/pages/briar/pages/tools/pages/three/index.tsx
+++ packages/briar-frontend/src/pages/briar/pages/tools/pages/three/index.tsx
@@ -3,17 +3,12 @@ import React from 'react';
 
 import { useThreeScene } from './hooks/useThreeScene';
 
-interface ThreeJSSceneProps {
-	width?: number;
-	height?: number;
-	modelUrl: string;
-}
+const width = 500;
+const height = 500;
+const modelUrl =
+	'https://file.yzcdn.cn/upload_files/yz-file/2025/04/08/lsCNwrk1ZUtlezJe3e5G7b2Nl1_5.glb';
 
-const ThreeJSScene: React.FC<ThreeJSSceneProps> = ({
-	width = 500,
-	height = 500,
-	modelUrl = 'https://file.yzcdn.cn/upload_files/yz-file/2025/04/08/lsCNwrk1ZUtlezJe3e5G7b2Nl1_5.glb'
-}) => {
+const ThreeJSScene = () => {
 	const containerRef = React.useRef<HTMLDivElement>(null);
 
 	useThreeScene(containerRef, {
