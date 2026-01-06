751b46b004ac826de6017ec022d567cfa47c0db9
zhanglei_zl <zhanglei_zl@youzan.com>
2026-01-06 20:22:10 +0800

    feat: add sun texture and enhance material properties in Sun component

--- packages/briar-frontend/src/pages/briar/pages/tools/pages/tail-wind/tabs/solar-system/components/Sun.tsx
+++ packages/briar-frontend/src/pages/briar/pages/tools/pages/tail-wind/tabs/solar-system/components/Sun.tsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/no-unknown-property */
+import { useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
 export const Sun: React.FC = () => {
 	const meshRef = useRef<THREE.Mesh>(null);
+	const SUN_URL =
+		'https://briar-shanghai-1309736035.cos.ap-shanghai.myqcloud.com/runtime-images/17677020207732k_sun.jpg';
+	const sunTexture = useTexture(SUN_URL);
+
+	useEffect(() => {
+		if (sunTexture) {
+			sunTexture.colorSpace = THREE.SRGBColorSpace;
+			sunTexture.anisotropy = 8;
+			sunTexture.needsUpdate = true;
+		}
+	}, [sunTexture]);
 
 	useFrame(({}) => {
 		if (meshRef.current) {
@@ -15,7 +27,7 @@ export const Sun: React.FC = () => {
 	return (
 		<mesh ref={meshRef} position={[0, 0, 0]}>
 			<sphereGeometry args={[4, 32, 32]} />
-			<meshBasicMaterial color="#FDB813" />
+			<meshBasicMaterial color="#ffffff" map={sunTexture} />
 			{/* Light Source from Sun */}
 			<pointLight intensity={2} distance={100} decay={1} color="#ffffff" />
 			{/* Ambient glow mesh */}
